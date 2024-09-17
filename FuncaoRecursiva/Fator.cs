28249232b2f89fe943c081bdbdebe5fdf218d477
Juliano Martins de Souza <julianopoamartins@gmail.com>
2024-09-17 11:42:53 -0300

    Adicionar arquivos de projeto.

---
+++ FuncaoRecursiva/Fator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoRecursiva {
+    static internal class Fator {
+
+        public static int Fatorial(int num) {
+            if(num <= 0){
+                return 1;
+            }
+
+            return num * Fatorial(num - 1);
+        }
+
+    }
+}
---
+++ FuncaoRecursiva/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuncaoRecursiva {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Title = "Programado por JulianoMarthins";
+
+            Console.WriteLine("-=-=-=-=-= Calculadora de Fatoriais -=-=-=-=-=\n");
+            Console.Write("Digite o número a ser calculado: ");
+
+            int num = int.Parse(Console.ReadLine());
+
+            int resultado = Fator.Fatorial(num);
+
+            Console.WriteLine($"O fator de {num} foi {resultado}\n");
+            
+
+        }
+    }
+}
